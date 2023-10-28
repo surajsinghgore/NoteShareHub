5f4aa012bf136a848e050e5f10ebbbb595e71b37
surajsinghgore <surajthakurrs45@gmail.com>
2023-10-28 13:27:05 +0530

    bug fix

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,6 @@ import { Providers } from "../redux/provider";
 import AuthProvider from "./components/authProvider/AuthProvider";
 import Header from "@/layout/Header";
 import LeftSideMenu from "@/layout/LeftSideMenu";
-import TopLoader from "@/components/TopLoader";
 
 export default function RootLayout({ children }) {
   return (
