8165aec3c69148c7e8ffe6306ac7bb1209901eb6
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-01 09:49:15 +0530

    post ui created successully

---
+++ Features/EnablePostOptions/index.js
@@ -0,0 +1,174 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBookmark,
+  faComment,
+  faEllipsis,
+  faShareNodes,
+  faThumbsDown,
+  faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons";
+import Image from "next/legacy/image";
+import { useState, useEffect, useRef } from "react";
+
+export default function Index(props) {
+    const dropdown = useRef(null);
+
+  const { Data } = props;
+  const [optionState, setOptionsState] = useState(false);
+  const enableOption = () => {
+    setOptionsState(!optionState);
+  };
+//   disable on outside click
+  useEffect(() => {
+    // only add the event listener when the dropdown is opened
+    if (!optionState) return;
+    function handleClick(event) {
+      if (dropdown.current && !dropdown.current.contains(event.target)) {
+        setOptionsState(false);
+      }
+    }
+    window.addEventListener("click", handleClick);
+    // clean up
+    return () => window.removeEventListener("click", handleClick);
+  }, [optionState]);
+  return (
+ 
+            <div className="post" ref={dropdown}>
+              {/* top section */}
+              <div className="top_section">
+                {/* hide menu on click */}
+                {optionState && (
+                  <div className="hide_menu" >
+                    <li>
+                      <div className="hidemenu_icons">
+                        <FontAwesomeIcon
+                          icon={faBookmark}
+                          className="hidemenu_icon"
+                        />
+                      </div>
+                      <div className="hidemenu_desc">
+                        <h2>Save Post</h2>
+                        <h3>Add this to your saved items</h3>
+                      </div>
+                    </li>
+
+                    <li>
+                      <div className="hidemenu_icons">
+                        <FontAwesomeIcon
+                          icon={faBookmark}
+                          className="hidemenu_icon"
+                        />
+                      </div>
+                      <div className="hidemenu_desc">
+                        <h2>Save Post</h2>
+                        <h3>Add this to your saved items</h3>
+                      </div>
+                    </li>
+
+                    <li>
+                      <div className="hidemenu_icons">
+                        <FontAwesomeIcon
+                          icon={faBookmark}
+                          className="hidemenu_icon"
+                        />
+                      </div>
+                      <div className="hidemenu_desc">
+                        <h2>Save Post</h2>
+                        <h3>Add this to your saved items</h3>
+                      </div>
+                    </li>
+                  </div>
+                )}
+
+                {/* profile */}
+                <div className="image_profile">
+                  <Image
+                    src={Data.image}
+                    alt={Data.media}
+                    layout="fill"
+                    className="profile_image"
+                    priority
+                  />
+                </div>
+                {/* User Name */}
+                <div className="user_detail">
+                  <h2>{Data.name}</h2>
+                  <h3>{Data.time}</h3>
+                </div>
+
+                {/* option btn */}
+                <div className="option" onClick={() => enableOption()}>
+                  <div className="post_icon">
+                    <FontAwesomeIcon icon={faEllipsis} />
+                  </div>
+                </div>
+              </div>
+
+              {/* description */}
+              <div className="post_description">
+                <p>{Data.description.split(0,168)}
+              
+                  <span>See More</span>
+                </p>
+              </div>
+
+              {/* media */}
+              <div className="media">
+                <div className="media_container">
+                  <Image
+                    src= {Data.media}
+                    alt={Data.media}
+                    layout="fill"
+                    className="media_image1"
+                    priority
+                  />
+                </div>
+              </div>
+
+              {/* bottom section */}
+              <div className="bottom_section">
+                {/* like */}
+                <div className="like">
+                  <div className="bottom_like_icon">
+                    <FontAwesomeIcon
+                      icon={faThumbsUp}
+                      className="bottom_icon"
+                    />
+                  </div>
+                  <div className="count">{Data.like}</div>
+                </div>
+
+                {/* dislike */}
+                <div className="like">
+                  <div className="bottom_like_icon dislike">
+                    <FontAwesomeIcon
+                      icon={faThumbsDown}
+                      className="bottom_icon"
+                    />
+                  </div>
+                  <div className="count">{Data.dislike}</div>
+                </div>
+
+                {/* comment */}
+                <div className="like">
+                  <div className="bottom_like_icon comment">
+                    <FontAwesomeIcon icon={faComment} className="bottom_icon" />
+                  </div>
+                  <div className="count">{Data.comment}</div>
+                </div>
+
+                {/* share */}
+                <div className="like share">
+                  <div className="bottom_like_icon">
+                    <FontAwesomeIcon
+                      icon={faShareNodes}
+                      className="bottom_icon"
+                    />
+                  </div>
+                  <div className="count">SHARE</div>
+                </div>
+              </div>
+            </div>
+          );
+       
+}
---
+++ components/Card/HomePostCard.js
@@ -0,0 +1,241 @@
+const Data = [
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 1,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "22 k",
+    dislike: "32 k",
+    comment: "12 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 2,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 3,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 19,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 4,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 5,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 6,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 7,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 8,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 9,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 10,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 11,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 12,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 13,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 14,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 15,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 16,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 17,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+  {
+    image: "/profile.webp",
+    name: "suraj singh",
+    time: "3 hours before",
+    id: 18,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam, et repellendus ducimus tenetur sapiente ullam dolorum tempora neque nobis expedita omnis",
+    media: "/img.jpg",
+    like: "2 k",
+    dislike: "3 k",
+    comment: "1 k",
+  },
+];
+import Card from "../../Features/EnablePostOptions";
+export default function HomePostCard() {
+  return (
+    <>
+      {Data.length > 0 &&
+        Data.map((item) => {
+          return <Card Data={item} key={item.id} />;
+        })}
+    </>
+  );
+}
