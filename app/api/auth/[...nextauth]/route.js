f068b8f8e8922077e1d94bf7923636dc042cbe23
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-04-18 15:31:10 +0530

    login with google again and again error solved

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,12 +2,15 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler=NextAuth({
+
   providers:[
     GoogleProvider({
+ 
       clientId:process.env.NEXT_PUBLIC_GOOGLE_APP_CLIENT_ID,
-      clientSecret:process.env.NEXT_PUBLIC_GOOGLE_APP_CLIENT_SECRET})
-  ],
-  secret: process.env.NEXT_PUBLIC_NEXT_AUTH_SECRET_KEY,
+      clientSecret:process.env.NEXT_PUBLIC_GOOGLE_APP_CLIENT_SECRET,
+   
+     })
+  ], secret: process.env.NEXT_PUBLIC_NEXT_AUTH_SECRET_KEY,
 })
 
 
--- app/login/page.js
+++ app/login/page.js
@@ -1,81 +1,67 @@
-"use client";
+'use client';
 
-import Image from "next/legacy/image";
-import style from "./login.module.css";
-import { signIn, useSession } from "next-auth/react";
-import LoadingBar from "react-top-loading-bar";
+import Image from 'next/legacy/image';
+import style from './login.module.css';
+import {  signIn, useSession } from "next-auth/react"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import ClientLoginVerify from '@/middleware/ClientLoginVerify';
 
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setClientData } from "../../redux/slice/ClientLoginInfo";
-import { clientLoginState } from "../../redux/slice/ClientLoginState";
 export default function Page() {
-  const [progress, setProgress] = useState(100);
-  const dispatch = useDispatch();
-  const { push } = useRouter();
-  const router = useRouter();
-  const session = useSession();
+    const { push } = useRouter();
+    const router = useRouter();
+    const session=useSession();
+    const LoginWithGoogle=async()=>{
 
-  const loginState = useSelector((state) => state.clientLoginState);
-  // page not allowed once login In
-  if (loginState.state) {
-    router.back();
-  }
-
-  const LoginWithGoogle = async () => {
-    setProgress(30);
-    await signIn("google");
-    setProgress(60);
-    sendDataToDb();
-  };
-
-  const sendDataToDb = async () => {
-    sendDataToDb(70)
-    let name = await session.data.user.name;
-    let email = await session.data.user.email;
-    let image = await session.data.user.image;
-    dispatch(setClientData({ name, email, image }));
-    if (name != undefined || email != undefined || image != undefined) {
-      let res = await fetch("/api/clientLogin", {
-        method: "POST",
-        body: JSON.stringify({ name, email, image }),
-      });
-      setProgress(100);
-      if (res.status == 200) {
-        localStorage.setItem("clientLogin", true);
-        dispatch(clientLoginState(true));
-        push("/");
-      }
+        signIn("google")
     }
-  };
+
+    const sendDataToDb=async()=>{
+        let name=session.data.user.name;
+        let email=session.data.user.email;
+        let image=session.data.user.image;
+        if((name!=undefined)||(email!=undefined)||(image!=undefined)){
+        let res=await fetch('/api/clientLogin',{
+            method:"POST",
+            body:JSON.stringify({name,email,image})
+        })
+        if(res.status==200){
+            localStorage.setItem('clientLogin',true);
+            push('/');
+
+            return;
+        }
+        
+    }
+    }
+    useEffect(()=>{
+
+        if(session.data!=undefined){      
+        let name=session.data.user.name;
+        let email=session.data.user.email;
+        let image=session.data.user.image;
+        if((name!=undefined)||(email!=undefined)||(image!=undefined)){
+            sendDataToDb()
+        }
+
+        if (localStorage.getItem("clientLogin")) {
+          push('/');
+          }
+    }
+    },[session.data]);
+    
+
+
 
   return (
-    <>
- <LoadingBar
-        color="#242c3f"
-        progress={progress}
-        onLoaderFinished={() => setProgress(progress)}
-        height={6}
-      />
-
     <div className={style.login}>
-      <div
-        className={style.googleBtn}
-        title="Login With Google"
-        onClick={() => LoginWithGoogle()}
-      >
-        <div className={style.googleImage}>
-          <Image
-            src="/google.png"
-            alt="google image login btn"
-            layout="fill"
-            priority
-          />
-        </div>
-        <div className={style.title}>Login with Google</div>
+    <ClientLoginVerify />
+      <div className={style.googleBtn} title='Login With Google'  onClick={()=>LoginWithGoogle()}>
+<div className={style.googleImage}>
+<Image src="/google.png" alt="google image login btn" layout='fill' priority/>
+</div>
+<div className={style.title}>Login with Google</div>
       </div>
     </div>
-    </>
-  );
+  )
 }
